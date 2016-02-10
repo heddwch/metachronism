f32ada97b7f2652a2e8a1f07c0c529ad38835511
Quinn Evans <heddwch@lambdaos.org>
2016-02-10 14:34:14 -0700

    Rewrote StdioDevice to be multithreaded.

--- src/main.rs
+++ src/main.rs
@@ -7,42 +7,235 @@ use std::io::{ self, Read, Write };
 use std::env;
 use std::str::FromStr;
 use std::fs::File;
-use std::sync::{ Arc, Mutex };
+use std::sync::{ Arc, Condvar, Mutex };
+use std::sync::atomic::{ AtomicBool, AtomicUsize, Ordering };
+use std::thread;
 
+trait ConcurrentDevice {
+    fn run(&mut self, die: Arc<AtomicBool>);
+}
+
+const STATUS_READY_READ: usize = 1;
+const STATUS_READY_WRITE: usize = 2;
+
+const BUF_LENGTH: usize = 0x100;
+
+struct StdioControl {
+    device: StdioDevice,
+}
+
+impl StdioControl {
+    fn new(device: StdioDevice) -> StdioControl {
+        StdioControl {
+            device: device,
+        }
+    }
+}
+
+impl Z80IODevice for StdioControl {
+    fn read_in(&self) -> u8 {
+        (self.device.status.load(Ordering::SeqCst) & 0xff) as u8
+    }
+    fn write_out(&mut self, value: u8) {
+    }
+}
+
+struct StdioData {
+    device: StdioDevice,
+}
+
+impl StdioData {
+    fn new(device: StdioDevice) -> StdioData {
+        StdioData {
+            device: device,
+        }
+    }
+}
+
+impl Z80IODevice for StdioData {
+    fn read_in(&self) -> u8 {
+        let status = self.device.status.load(Ordering::SeqCst);
+        if (status & STATUS_READY_READ) != 0 {
+            let mut buffer = match self.device.read_buffer.lock() {
+                Ok(x) => x,
+                Err(err) => {
+                    panic!("StdioDevice buffer mutex poisoned: {}", err);
+                }
+            };
+            if buffer.len() == 1 {
+                self.device.status.fetch_and(!STATUS_READY_READ, Ordering::SeqCst);
+            }
+            let byte = buffer.remove(0);
+            byte
+        } else {
+            let _ = writeln!(io::stderr(), "Attempted to read StdioDevice when it wasn't ready.");
+            0
+        }
+    }
+    fn write_out(&mut self, value: u8) {
+        let status = self.device.status.load(Ordering::SeqCst);
+        if (status & STATUS_READY_WRITE) != 0 {
+            let mut buffer = match self.device.writer_state.buffer.lock() {
+                Ok(x) => x,
+                Err(err) => {
+                    panic!("StdioDevice buffer mutex poisoned: {}", err);
+                },
+            };
+            buffer.push(value);
+            let mut have_data = self.device.writer_state.have_data.lock().unwrap();
+            self.device.status.fetch_and(!STATUS_READY_WRITE, Ordering::SeqCst);
+            *have_data = true;
+            self.device.writer_state.have_data_cond.notify_one();
+        } else {
+            let _ = writeln!(io::stderr(), "StdioDevice written to when not ready.");
+        }
+    }
+}
+
+struct StdioReader {
+    device: StdioDevice,
+    stdin: io::Stdin,
+}
+
+impl StdioReader {
+    fn new(device: StdioDevice) -> StdioReader {
+        StdioReader {
+            device: device,
+            stdin: io::stdin(),
+        }
+    }
+}
+
+impl ConcurrentDevice for StdioReader {
+    fn run(&mut self, die: Arc<AtomicBool>) {
+        let mut buf: [u8; BUF_LENGTH] = [0; BUF_LENGTH];
+        loop {
+            if die.load(Ordering::Acquire) { break };
+            let count = match self.stdin.read(&mut buf) {
+                Ok(x) => x,
+                Err(err) => {
+                    let _ = writeln!(io::stderr(), "Error reading from stdin: {}", err);
+                    0
+                },
+            };
+            if count > 0 {
+                let mut buffer = match self.device.read_buffer.lock() {
+                    Ok(x) => x,
+                    Err(err) => panic!("StdioDevice read buffer mutex poisoned: {}", err),
+                };
+                for i in 0..count {
+                    buffer.push(buf[i]);
+                }
+                self.device.status.fetch_or(STATUS_READY_READ, Ordering::SeqCst);
+            }
+        }
+    }
+}
+
+struct StdioWriter {
+    device: StdioDevice,
+    stdout: io::Stdout,
+}
+
+impl StdioWriter {
+    fn new(device: StdioDevice) -> StdioWriter {
+        StdioWriter {
+            device: device,
+            stdout: io::stdout(),
+        }
+    }
+}
+
+impl ConcurrentDevice for StdioWriter {
+    fn run(&mut self, die: Arc<AtomicBool>) {
+        let mut have_data = self.device.writer_state.have_data.lock().unwrap();
+        self.device.status.fetch_or(STATUS_READY_WRITE, Ordering::SeqCst);
+        loop {
+            while !*have_data {
+                have_data = self.device.writer_state.have_data_cond.wait(have_data).unwrap();
+            }
+            if die.load(Ordering::Acquire) { break };
+            let mut buffer = self.device.writer_state.buffer.lock().unwrap();
+            let mut buf: [u8; BUF_LENGTH] = [0; BUF_LENGTH];
+            loop {
+                let mut count = 0;
+                {
+                    let mut items = buffer.iter();
+                    for i in 0..BUF_LENGTH {
+                        match items.next() {
+                            Some(x) => {
+                                buf[i] = *x;
+                                count += 1;
+                            },
+                            None => {
+                                break
+                            },
+                        }
+                    };
+                }
+                match self.stdout.write_all(&buf[..count]) {
+                    Ok(()) => {
+                        if buffer.len() >= count{
+                            *buffer = buffer[count..].to_vec();
+                        } else {
+                            *buffer = Vec::new();
+                        }
+                    },
+                    Err(err) => {
+                        let _ = writeln!(io::stderr(), "Error writing to stdout: {}", err);
+                    },
+                }
+                self.stdout.flush().unwrap();
+                if count == 0 { break; }
+            }
+            *have_data = false;
+            self.device.status.fetch_or(STATUS_READY_WRITE, Ordering::SeqCst);
+        }
+    }
+}
+
+struct StdioWriterState {
+    buffer: Mutex<Vec<u8>>,
+    have_data: Mutex<bool>,
+    have_data_cond: Condvar,
+}
+
+impl StdioWriterState {
+    fn new() -> StdioWriterState {
+        StdioWriterState {
+            buffer: Mutex::new(Vec::new()),
+            have_data: Mutex::new(false),
+            have_data_cond: Condvar::new(),
+        }
+    }
+}
+
+#[derive(Clone)]
 struct StdioDevice {
-    stdin: Mutex<io::Stdin>,
-    stdout: Mutex<io::Stdout>,
+    pub status: Arc<AtomicUsize>,
+    read_buffer: Arc<Mutex<Vec<u8>>>,
+    writer_state: Arc<StdioWriterState>,
 }
 
 impl StdioDevice {
     fn new() -> StdioDevice {
         StdioDevice {
-            stdin: Mutex::new(io::stdin()),
-            stdout: Mutex::new(io::stdout())
+            status: Arc::new(AtomicUsize::new(0)),
+            read_buffer: Arc::new(Mutex::new(Vec::new())),
+            writer_state: Arc::new(StdioWriterState::new()),
         }
     }
-}
-
-impl Z80IODevice for StdioDevice {
-    fn read_in(&self) -> u8 {
-        let mut buf: [u8; 1] = [0];
-        let mut stdin = match self.stdin.lock() {
-            Ok(x) => x,
-            Err(err) => {
-                panic!("StdioDevice mutex poisoned: {}", err);
-            },
-        };
-        let _ = stdin.read(&mut buf);
-        buf[0]
+    fn get_control_port(&self) -> StdioControl {
+        StdioControl::new(self.clone())
     }
-    fn write_out(&mut self, value: u8) {
-        let mut stdout = match self.stdout.lock() {
-            Ok(x) => x,
-            Err(err) => {
-                panic!("StdioDevice mutex poisoned: {}", err);
-            },
-        };
-        let _ = stdout.write(&[value]);
+    fn get_data_port(&self) -> StdioData {
+        StdioData::new(self.clone())
+    }
+    fn get_reader(&self) -> StdioReader {
+        StdioReader::new(self.clone())
+    }
+    fn get_writer(&self) -> StdioWriter {
+        StdioWriter::new(self.clone())
     }
 }
 
@@ -57,7 +250,6 @@ const NUM_BANKS: u8 = 1;
 fn main() {
     let mut num_banks = NUM_BANKS;
     let mut stderr = std::io::stderr();
-    let mut device = StdioDevice::new();
     let memory;
     let mut mmu;
     {
@@ -172,12 +364,33 @@ fn main() {
         }
     }
     let mut cpu = Z80::new(&mut mmu);
-    cpu.install_device(0, &mut device);
-    cpu.install_device(1, &mut mmu.bank_registers[0]);
-    cpu.install_device(2, &mut mmu.bank_registers[1]);
-    cpu.install_device(3, &mut mmu.bank_registers[2]);
-    cpu.install_device(4, &mut mmu.bank_registers[3]);
+    cpu.install_device(0, &mut mmu.bank_registers[0]);
+    cpu.install_device(1, &mut mmu.bank_registers[1]);
+    cpu.install_device(2, &mut mmu.bank_registers[2]);
+    cpu.install_device(3, &mut mmu.bank_registers[3]);
+
+    let device = StdioDevice::new();
+    cpu.install_device(4, &mut device.get_control_port());
+    cpu.install_device(5, &mut device.get_data_port());
+
+    let die = Arc::new(AtomicBool::new(false));
+    let mut device_threads = Vec::new();
+    {
+        let mut reader = device.get_reader();
+        let die = die.clone();
+        device_threads.push(thread::spawn(move || reader.run(die)));
+    }
+    {
+        let mut writer = device.get_writer();
+        let die = die.clone();
+        device_threads.push(thread::spawn(move || writer.run(die)));
+    }
+
     let cpu = Arc::new(&cpu);
-    
     let _ = cpu.execute(0);
+    die.store(true, Ordering::Release);
+    for thread in device_threads {
+        let _ = thread.join().unwrap();
+    }
+    println!("Exiting successfully.")
 }
