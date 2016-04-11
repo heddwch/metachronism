fc5f7c0fbbdeaaff40def973730b01769f3f1c6b
Quinn Evans <yoshizuki@gmail.com>
2016-04-10 20:21:18 -0600

    Separate src and obj disks, leave YAZE open after.

--- bios/build.sh
+++ bios/build.sh
@@ -20,10 +20,17 @@ c:
 EOF
 for file in $S_SRC/${VERSION:=2.2}/*.z80
 do
-    echo Z80ASM $(basename ${file} .z80)/A >> ${VERSION}
-    echo W $(basename ${file} .z80).COM >> ${VERSION}
+    echo Z80ASM $(basename ${file} .z80).CDD/A >> ${VERSION}
+    echo W D:$(basename ${file} .z80).COM >> ${VERSION}
 done
-echo e >> ${VERSION}
+cat >> ${VERSION} <<EOF
+d:
+;Build is complete.
+;Sources are on C:
+;Output is on D:
+;Output has also been written out to unix build directory.
+;Submit 'E' to exit
+EOF
 
 cd $BUILD_DIR/disks
 BOOT_UTILS="BOOT_UTILS${VERSION}.ydsk"
@@ -40,8 +47,8 @@ cp t:${BUILD_DIR}/profiles/${VERSION} a:profile.sub
 quit
 EOF
 cat > ${VERSION}.cdm <<EOF
-create build${VERSION}.ydsk
-mount a build${VERSION}.ydsk
+create src${VERSION}.ydsk
+mount a src${VERSION}.ydsk
 EOF
 for file in ${S_SRC}/common/*
 do
@@ -53,12 +60,20 @@ do
 done
 echo quit >> ${VERSION}.cdm
 cdm < ${VERSION}.cdm
+if [ -n obj${VERSION}.ydsk ]
+then
+    cdm <<EOF
+create obj${VERSION}.ydsk
+quit
+EOF
+fi
 
 cd $BUILD_DIR
 cat > build.rc <<EOF
 mount a disks/${BOOT_UTILS}
 mount b disks/CPM3_SYS.ydsk
-mount c disks/build${VERSION}.ydsk
+mount c disks/src${VERSION}.ydsk
+mount d disks/obj${VERSION}.ydsk
 go
 EOF
 
