3881e3e927900ad217e42cce4dc6a91280c18cd6
Quinn Evans <yoshizuki@gmail.com>
2016-04-13 14:25:43 -0600

    Add support for libraries to build.sh, split conio

--- bios/build.sh
+++ bios/build.sh
@@ -15,52 +15,62 @@ then
 fi
 
 read_dep () {
-    local FILE
-    while read FILE
+    local MODULE
+    while read MODULE
     do
 	local DEP
-	DEP=${BUILD_DIR}/deps/$(echo ${FILE} | tr [:upper:] [:lower:]).dep
+	DEP=${BUILD_DIR}/deps/$(echo ${MODULE} | tr [:upper:] [:lower:]).dep
 	if [ -e ${DEP} ]
 	then
-	    read_dep ${DEP}
+	    read TYPE < ${DEP}
+	    if [${TYPE} != "LIB" ]
+	    then
+		read_dep ${DEP}
+	    fi
 	fi
-	DEPS=${DEPS:+${DEPS}$'\\n'}${FILE}
+	DEPS="${DEPS} ${MODULE}"
     done <<EOF
 $(tail -n +2 ${1})
 EOF
 }
 
 cd ${BUILD_DIR}/deps
-for file in $S_SRC/${VERSION:=2.2}/*.z80 $S_SRC/common/*.z80
+for file in $S_SRC/${VERSION:=2.2}/*.z80 $S_SRC/${VERSION:=2.2}/*.lib $S_SRC/common/*.z80 $S_SRC/common/*.lib
 do
-    DEP=$(basename ${file} .z80).dep
+    DEP=$(basename ${file} | awk -F . '{print $(NF - 1)}').dep
     DEPS=
-    COM=
+    TYPE=REL
     DEPS=
     IFS=$' \t\n\r'
     while read OP OPERAND
     do
 	case $(echo ${OP} | tr [:lower:] [:upper:]) in
 	    IMPORT)
-		IFS=$', \t'
-		for dep in ${OPERAND}
-		do
-		    DEPS=${DEPS:+${DEPS}$'\n'}${dep}
-		done
+		IFS=$', \t\n\r'
+		if [ ${OPERAND} ]
+		then
+		    for dep in ${OPERAND}
+		    do
+			DEPS="${DEPS:+${DEPS} }${dep}"
+		    done
+		else
+		    echo "${file}: IMPORT missing operand" >&2
+		fi
 		IFS=$' \t\n\r'
 		;;
 	    COM)
-		COM=YES
+		TYPE=COM
+		;;
+	    LIB)
+		TYPE=LIB
 		;;
 	esac
     done < ${file}
-    if [ ${COM} ]
-    then
-	echo "COM" > ${DEP}
-    else
-	echo "REL" > ${DEP}
-    fi
-    echo ${DEPS} >> ${DEP}
+    echo ${TYPE} > ${DEP}
+    for dep in ${DEPS}
+    do
+	echo ${dep} >> ${DEP}
+    done
 done
 
 cd ${BUILD_DIR}/profiles
@@ -73,22 +83,52 @@ do
     echo Z80ASM $(basename ${file} .z80).CDD/M >> ${VERSION}
 done
 echo d: >> ${VERSION}
-for file in $S_SRC/${VERSION:=2.2}/*.z80 $S_SRC/common/*.z80
+for file in ${BUILD_DIR}/deps/*.dep
 do
-    DEP=${BUILD_DIR}/deps/$(basename ${file} .z80).dep
     DEPS=
-    read TYPE < ${DEP}
+    read TYPE < ${file}
+    if [ ${TYPE} = "LIB" ]
+    then
+	read_dep ${file}
+	printf "LIB $(basename ${file} .dep)=" >> ${VERSION}
+	DEPS=$(for dep in ${DEPS}
+	do
+	    echo ${dep}
+	done | sort | uniq)
+	for dep in ${DEPS}
+	do
+	    printf "${dep}," >> ${VERSION}
+	done
+	truncate -s -1 ${VERSION}
+	printf "\n" >> ${VERSION}
+    fi
+done
+
+for file in ${BUILD_DIR}/deps/*.dep
+do
+    DEPS=
+    read TYPE < ${file}
     if [ ${TYPE} = "COM" ]
     then
-	read_dep ${DEP}
-        printf "LINK $(basename ${file} .z80)" >> ${VERSION}
+	read_dep ${file}
+        printf "LINK $(basename ${file} .dep)[L0000]" >> ${VERSION}
 	DEPS=$(echo ${DEPS} | sort | uniq)
 	for dep in ${DEPS}
 	do
-	    printf ",${dep}" >> ${VERSION}
+	    LIB=
+	    FILE=${BUILD_DIR}/deps/$(echo ${dep} | tr [:upper:] [:lower:]).dep
+	    if [ -e ${FILE} ]
+	    then
+		read TYPE < ${FILE}
+		if [ ${TYPE} = "LIB" ]
+		then
+		    LIB=YES
+		fi
+		printf ",${dep}${LIB:+[s]}" >> ${VERSION}
+	    fi
 	done
-	printf "[L0000]\n" >> ${VERSION}
-	echo "W $(basename ${file} .z80).COM B" >> ${VERSION}
+	printf "\n" >> ${VERSION}
+	echo "W $(basename ${file} .dep).COM B" >> ${VERSION}
     fi
 done
 cat >> ${VERSION} <<EOF
